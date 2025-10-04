b038e669eb808afc7642ec2378e54242c1a01829
Rakshita manjunath yachalgar <01fe23bca255@kletech.ac.in>
2025-10-04 10:53:48 +0530

    production issues

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.https://toyesaudveebfcafwcbr.supabase.co,
-  process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRveWVzYXVkdmVlYmZjYWZ3Y2JyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk0OTQxMzYsImV4cCI6MjA3NTA3MDEzNn0.MUi8KdfUzZDxMLS8KXsMKt3nSNndTKs5FkN2fTNog1Y
+  process.env.REACT_APP_SUPABAS_URLE,
+  process.env.REACT_APP_SUPABASE_ANON_KEY
 );
 
 export default supabase;
